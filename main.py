dae3e9e709e80b3c0fb11442f6acf0d62cff7d8f
nekromantb <nekromantbit@gmail.com>
2023-07-11 20:13:35 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+"""
+    Task №1: Data collecting from Onlíner
+
+    Description
+
+    This script collects information from https://www.onliner.by/
+    about last 2 added flats with 2 rooms in Minsk town.
+
+    Additional requirement:
+    Add data about phone number (if exist) and description
+
+    Input
+    Script inputs: None;
+
+    Output
+    Script outputs: Json file with flats info.
+
+    Output data format:
+    [ { "address": "Минск, улица Кропоткина, 84",
+    “created_at”: “2023-05-17 14:41:50”,
+    "latlon": [55.876279, 37.331549],
+    "photo": "https://content.onliner.by/apartment_rentals/1676580/600x400/8.jpeg",
+    ”price_usd”: 200,
+    “price_byn”: 600,
+    “url”: “https://r.onliner.by/ak/apartments/824321”,
+    “phone_number”: “+375(44)547-12-85”
+    “description”: “Описание объявления” }, ... ]
+
+    Run
+
+    To run the script make sure all the requirements are met and just run the script from the IDE
+"""
+
+from collector import collector
+
+
+def main():
+    collector()
+
+
+if __name__ == '__main__':
+    main()
